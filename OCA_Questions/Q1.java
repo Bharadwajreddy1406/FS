db1efbdd151660de762f3cda5a3a1c051cad47a6
Bharadwajreddy1406 <bharadwajreddy146@gmail.com>
2025-02-26 13:08:32 +0530

    add initial implementation of Q1.java with a coding question about class structure

---
+++ OCA_Questions/Q1.java
@@ -0,0 +1,25 @@
+
+// 10) Given the file Magnet.java below, which of the marked lines can 
+// you independently insert the line public String color;
+// into and still have the code compile?
+
+
+// // line a1
+// public class Magnet {
+// // line a2
+// public void attach() {
+// // line a3
+// }
+// // line a4
+// }
+// A. a1 and a3
+// B. a2 and a4
+// C. a2, a3, and a4
+// D. a1, a2, a3, and a4
+
+
+public class Q1{
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
